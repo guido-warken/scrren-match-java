1bce46219428c4b5f13ba738f7671ba7dbfbd3df
Guido <guido.warken@gmail.com>
2024-11-30 10:57:36 -0300

    Commit da primeira aula

---
+++ SRC/Filme.java
@@ -0,0 +1,24 @@
+class Filme {
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    double somadasAvaliacoes;
+    int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+
+    void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Lançado em: " + anoDeLancamento);
+        System.out.println("Total de avaliações: " + totalDeAvaliacoes);
+        System.out.println("Avaliação média: %.2f".formatted(calculaMedia()));
+    }
+
+    void avalia(double nota) {
+        somadasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double calculaMedia() {
+        return somadasAvaliacoes / totalDeAvaliacoes;
+    }
+}
---
+++ SRC/Principal.java
@@ -0,0 +1,22 @@
+class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.anoDeLancamento = 1970;
+        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.nome = "Poderoso Chefão";
+        meuFilme.incluidoNoPlano = true;
+
+        meuFilme.avalia(9.5);
+        meuFilme.avalia(9);
+        meuFilme.avalia(10);
+
+        meuFilme.exibeFichaTecnica();
+
+        meuFilme.avalia(8);
+        meuFilme.avalia(9);
+        meuFilme.avalia(9.5);
+
+        meuFilme.exibeFichaTecnica();
+
+    }
+}
